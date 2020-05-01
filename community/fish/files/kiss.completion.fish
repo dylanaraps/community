d5916a03d1b56308a72160d2991050f0df5bf16c
Camille <camille@airmail.cc>
2020-05-01 07:47:29 +0200

    fish: update to version 3.1.2 (#763)

--- community/fish/files/kiss.completion.fish
+++ community/fish/files/kiss.completion.fish
@@ -27,6 +27,8 @@ function __fish_kiss_no_subcommand -d 'Test if kiss has yet to be given the comm
     return 0
 end
 
+# Standard commands.
+
 complete -c kiss -f -n __fish_kiss_no_subcommand -a alternatives -d 'List and swap to alternatives'
 complete -c kiss -f -n '__fish_seen_subcommand_from a alternatives' -a '(kiss alternatives)'
 
@@ -52,3 +54,43 @@ complete -c kiss -f -n __fish_kiss_no_subcommand -a search -d 'Search for a pack
 complete -c kiss -f -n __fish_kiss_no_subcommand -a update -d 'Check for updates'
 
 complete -c kiss -f -n __fish_kiss_no_subcommand -a version -d 'Package manager version'
+
+# Contrib commands.
+
+#complete -c kiss -f -n __fish_kiss_no_subcommand -a cargo-urlgen -d 'Generate sources for rust package'
+
+#complete -c kiss -f -n __fish_kiss_no_subcommand -a chbuild -d 'Create/destroy temporary chroot'
+
+#complete -c kiss -f -n __fish_kiss_no_subcommand -a chroot -d 'Enter a kiss chroot'
+
+#complete -c kiss -f -n __fish_kiss_no_subcommand -a depends -d 'Display a package\'s dependencies'
+
+#complete -c kiss -f -n __fish_kiss_no_subcommand -a depends-finder -d 'Find missing dependencies by parsing \'ldd\''
+
+#complete -c kiss -f -n __fish_kiss_no_subcommand -a export -d 'Turn an installed package into a KISS tarball'
+
+#complete -c kiss -f -n __fish_kiss_no_subcommand -a fork -d 'Copy a package\'s repository files into the current directory'
+
+#complete -c kiss -f -n __fish_kiss_no_subcommand -a link -d 'Link a repository file to another repository'
+
+#complete -c kiss -f -n __fish_kiss_no_subcommand -a maintainer -d 'Find the maintainer of a package'
+
+#complete -c kiss -f -n __fish_kiss_no_subcommand -a manifest -d 'Display all files owned by a package'
+
+#complete -c kiss -f -n __fish_kiss_no_subcommand -a manifest-tree -d 'Display all files owned by a package in a tree'
+
+#complete -c kiss -f -n __fish_kiss_no_subcommand -a new -d 'Create a boilerplate package'
+
+#complete -c kiss -f -n __fish_kiss_no_subcommand -a orphans -d 'List orphaned packages'
+
+#complete -c kiss -f -n __fish_kiss_no_subcommand -a outdated -d 'Check installed packages for updates'
+
+#complete -c kiss -f -n __fish_kiss_no_subcommand -a owns -d 'Check which package owns a file'
+
+#complete -c kiss -f -n __fish_kiss_no_subcommand -a repodepends -d 'Display a package\'s original dependencies'
+
+#complete -c kiss -f -n __fish_kiss_no_subcommand -a reset -d 'Remove all packages except for the base'
+
+#complete -c kiss -f -n __fish_kiss_no_subcommand -a revdepends -d 'Display packages which depend on package'
+
+#complete -c kiss -f -n __fish_kiss_no_subcommand -a size -d 'Show the size on disk for a package'
