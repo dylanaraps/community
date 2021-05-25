41afd5b1c9f2611141d89f656727f568b5e4534b
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2021-05-25 20:32:38 +0530

    [ungoogled]chromium: 91.0.4472.77
    Switch to system libs for 'libxml2' and 'libxslt'
    
    Fix loop to remove build scripts for system libs (unquote `$system`)
    
    Change logic in 'net/dns/BUILD.gn' to use portable source files instead of glibc-ism infested ones (https://github.com/chromium/chromium/commit/f79bc2e3b706140040f62ceecb55969a9fcd50e2)
    
    Update 'post-install' for vaapi changes (https://bugs.chromium.org/p/chromium/issues/detail?id=1207478)

--- community/chromium/files/chromium.sh
+++ community/chromium/files/chromium.sh
@@ -4,4 +4,5 @@ export CHROME_WRAPPER=/usr/lib/chromium/chromium
 
 CHROME_FLAGS="--enable-gpu-rasterization $CHROME_FLAGS"
 
+# shellcheck disable=2086
 exec /usr/lib/chromium/chromium $CHROME_FLAGS "$@"
